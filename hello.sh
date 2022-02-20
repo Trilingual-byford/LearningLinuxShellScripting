ab64cc73eca28611a987ef179b1d1b6e8f038abd
Byford Chen <niconicocbf@163.COM>
2022-02-21 00:05:45 +0900

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This is the first Bash Shell
+# Script name: Hello.sh
+# Written by: Byford Chen
+echo "Hello $LOGNAME.have a nice day !"
+echo "You are working in directory `pwd`."
+echo "You are working on a mahine called `uname -n`"
+echo "List of files in your directory is."
+ls # List files in the present working directory
+echo "bye for now $LOGNAME. The time is `date +%T`!"
